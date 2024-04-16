67129f1fea84f078cb3684b4a626d6a8a3717d46
Khoi <nguyenanhkhoi231202@gmail.com>
2024-04-16 18:07:01 +0700

    Segment tree + add Slide + add gitignore

--- ThucHanh/lythuyet/SegmentTreeSimulation-queryMax.cpp
+++ ThucHanh/lythuyet/SegmentTreeSimulation-queryMax.cpp
@@ -1,12 +1,14 @@
-
+#include<iostream>
+#include <cmath>
 using namespace std;
 
 
-const int MAX=1e5;
-const int MIN=-1e5;
+const int MAX=1e7;
+const int MIN=-1e7;
 int arr[MAX];
 int n,m;
 int i,j;
+string str;
 
 int makeNode(int si, int L, int R, int* st){
     if(L==R){
@@ -41,6 +43,19 @@ int getMax(int si, int L, int R, int* st){
     return max( getMax(si*2+1,L,mid,st), getMax(si*2+2,mid+1,R,st));
 }
 
+void update(int si, int L, int R, int* st,int i, int v){
+    if(L>R)return;
+    if(i<L || i>R)return;
+    if(L==R && L==i){
+        st[si] = max( v, st[si]);
+        return;
+    }
+    int mid=L+(R-L)/2;
+    update(si*2+1,L,mid,st,i,v);
+    update(si*2+2,mid+1,R,st,i,v);
+    st[si]=max(st[si*2+1],st[si*2+2]);
+    return;
+}
 
 int main(){
     freopen("input.txt", "r", stdin);
@@ -51,13 +66,20 @@ int main(){
     int* st = makeST();
     cin>>m;
 
-    for( int i=0; i<m; i++){
+    for( int k=0; k<m; k++){
+        cin>>str;
+        if(str=="get-max"){
         cin>>i>>j;
+        i--;
+        j--;
         cout<<getMax(0,0,n-1,st)<<endl;
+        }
+        else{
+            cin>>i>>j;
+            i--;
+            update(0,0,n-1,st,i,j);
+        }
     }
-    for (int i = 0; i < 25; i++)
-    {
-        cout << *(st + i) << ' ';
-    }
+
     return 0;
 }
